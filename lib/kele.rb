49ba5f25457989ff1607463518d26cd2ffaa47bd
aso4 <albertyso@gmail.com>
2016-11-21 18:53:20 -0500

    store auth token and raise ArgumentError for invalid credentials

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,10 +2,17 @@ require 'httparty'
 
 class Kele
   include HTTParty
-  base_uri "https://www.bloc.io/api/v1/"
+  base_uri 'https://www.bloc.io/api/v1/sessions'
 
-  def initialize(email, password)
-    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+  def initialize email, password
+    response = self.class.post 'https://www.bloc.io/api/v1/sessions', body: {email: email, password: password}
 
+    if response['auth_token'].nil?
+      raise ArgumentError.new 'Invalid email and/or password.'
+    else
+      @auth_token = response['auth_token']
+    end
   end
+
+
 end
